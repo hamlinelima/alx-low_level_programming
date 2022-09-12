9c2cecdb223aa9a553bd72dc00df11f318d971ae
hamlinelima <60290110+hamlinelima@users.noreply.github.com>
2022-09-12 19:16:07 +0200

    Variables if else while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+/**
+ * main - main entry of a program
+ *
+ * Return: return 0 (zero)
+ */
+int main(void)
+{
+	int x = 48, y, z;
+
+	while (x < 58)
+	{
+		y = x + 1;
+		while (y < 58)
+		{
+			z = y + 1;
+			while (z < 58)
+			{
+
+				if (y == z || x == z || x == y)
+				{
+					y++;
+					z++;
+					continue;
+				}
+				else
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if ((x == 55) && (y == 56) && (z == 57))
+					{
+						putchar('\n');
+						return (0);
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					z++;
+				}
+			}
+			y++;
+		}
+		x++;
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main is the entry point of the program
+ *
+ * Return: returns 0 (zero)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int x = 0;
+
+	while (x <= 25)
+	{
+		putchar(alphabet[x]);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
